71108ada4a9414d139191a634c849226659d4db9
zhangpeibisha <zhangpe0312@qq.com>
2019-06-20 13:24:41 +0800

    build命令拼写错误

--- start.sh
+++ start.sh
@@ -3,5 +3,6 @@
 echo 'hello word'
 run_port=10120
 monitor_port=10130
+jarName="ui-examination.jar"
 
-java -jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=${monitor_port} learn.jar --server.port=${run_port}
+java -jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=${monitor_port}  ${jarName} --server.port=${run_port}
