249f66e2936f957405d0087c1552ac7592223a9f
zhangpeibisha <zhangpe0312@qq.com>
2019-06-20 13:19:38 +0800

    build命令拼写错误

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # 关闭占用端口
 run_port=10130
 monitor_port=10120
-jar_name="ui_examination.jar"
+jar_name="ui-examination.jar"
 
 sudo fuser -k -n tcp ${run_port}
 sudo fuser -k -n tcp ${monitor_port}
@@ -14,6 +14,7 @@ docker rm ui-examination/container
 docker rmi ui-examination
 
 # 移动文件到目的位置
+pwd
 rm -f ${jar_name}
 mv ./target/${jar_name} ${jar_name}
 
